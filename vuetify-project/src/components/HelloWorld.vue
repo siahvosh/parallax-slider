5129b4a8d2848611245079d7ff8bc378bb452745
siahvosh <siablack370@gmail.com>
2024-02-17 20:51:43 +0330

    first cm

---
+++ vuetify-project/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card class="card-1"></v-card>
+
+</template>
+
+<script setup>
+  //
+</script>
+
+
+<style>
+
+.card-1{
+  width: 100%;
+  height: 100%;
+  background: red !important;
+}
+</style>
