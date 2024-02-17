c410ca10f6fd962ef8aa9fb472b5c8199f65a4d0
siahvosh <siablack370@gmail.com>
2024-02-17 22:03:09 +0330

    card-2

--- vuetify-project/src/components/HelloWorld.vue
+++ vuetify-project/src/components/HelloWorld.vue
@@ -2,12 +2,15 @@
     <div class="main">
       <div class="card-1">
         <v-img class="sld1-bg" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/sld1-bg.jpeg" alt=""></v-img>
-        <v-img class="car-cloud" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/carcloud.webp" alt=""></v-img>
-      </div>
-      <div class="card-2">
         <v-img class="sld2-bg" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/sld3-bg.webp" alt=""></v-img>
-        <v-img class="land" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/land.webp" alt=""></v-img>
+        <v-img class="sld2-bg" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/sld4-bg.webp" alt=""></v-img>
+        <v-img class="sld1-bg" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/sld2-bg.webp" alt=""></v-img>
       </div>
+<!--      <div class="card-2">-->
+<!--        <v-img class="car-cloud" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/carcloud.webp" alt=""></v-img>-->
+<!--        <v-img class="land" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/land.webp" alt=""></v-img>-->
+
+<!--      </div>-->
     </div>
 
 </template>
@@ -21,7 +24,6 @@
 
 .main{
   width: 100%;
-  height: 100%;
   background: black !important;
   position: relative;
   top: 50%;
@@ -29,21 +31,28 @@
   transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+
+
 }
 
-.card-1, .card-2{
-  position: relative;
+.card-1 {
+  position: relative !important;
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column ;
+  justify-content: initial;
+
+
 }
 
 .sld1-bg, .sld2-bg{
   object-fit: fill;
-  position: absolute !important;
   width: 100%;
-  height: 100%;
-  overflow: hidden;
-  border: 2px solid red;
+  display: flex;
+  justify-self: center;
+  /*border: 2px solid red;*/
 
 }
 .car-cloud{
