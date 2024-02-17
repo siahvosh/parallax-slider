1361cab5f262a046afcd08f52ab6245324371088
siahvosh <siablack370@gmail.com>
2024-02-17 21:36:43 +0330

    card-1

--- vuetify-project/src/components/HelloWorld.vue
+++ vuetify-project/src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
-    <v-card class="card-1"></v-card>
+    <div class="main">
+      <div class="card-1">
+      <v-img class="sld1-bg" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/sld1-bg.jpeg" alt=""></v-img>
+      <v-img class="car-cloud" src="file:///C:/Users/PC/Desktop/vue%20project/parallax-slider/vuetify-project/src/assets/carcloud.webp" alt=""></v-img>
+      </div>
+    </div>
 
 </template>
 
@@ -10,13 +15,38 @@
 
 <style>
 
-.card-1{
-  width: 50%;
-  height: 50%;
+.main{
+  width: 100%;
+  height: 100%;
   background: black !important;
   position: relative;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+}
+
+.card-1{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.sld1-bg{
+  object-fit: fill;
+  position: absolute !important;
+
+  width: 100%;
+  height: 100%;
+}
+.car-cloud{
+  position: absolute !important;
+  width: 30%;
+  height: 30%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
 }
 </style>
